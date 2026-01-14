2186b6fe4ccfed6b0af7828569389fd15a81df3f
Shakhzod <shakhzod.ds@gmail.com>
2026-01-14 13:01:10 +0900

    fix: train area location changed

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import dotenv from "dotenv"; // .env faylini o'qish uchun maxsus package
-
 dotenv.config(); // .env faylini yuklash:
 
-// console.log("After:", process.env);
-
-console.log("PORT:", process.env.PORT);
-
-console.log("MONGO_URL:", process.env.MONGO_URL);
+// console.log("process.env", process.env);
+// console.log("PORT:", process.env.PORT);
+// console.log("MONGO_URL:", process.env.MONGO_URL);
