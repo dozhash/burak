1dac1b21c615ccf5ee07adf1772ed353182746b1
Shakhzod <shakhzod.ds@gmail.com>
2026-01-17 19:17:10 +0900

    feat: create routing and member controller

---
+++ darsNotes/38_39.ts
@@ -0,0 +1,36 @@
+/* 38. Router vs Controller lar
+
+Dars Rejasi:
+    1. Routerlarni o'rganamiz
+    2. MVC architectual pattern ni o'rganamiz
+    3. Member controller larni hosil qilamiz 
+    4. Restaurant controller larni hosil qilamiz
+
+Lesson Flow:
+1. router.ts nomli file ochib unda routinglarni hosil qildik. Va u fayli app.ts fileda chaqirib oldik
+
+2. Bo'lim 2 da Architectural pattern lar haqida gaplashildi:
+    1) Avval clientlar yani browserlar dan kelayotgan requist ni bizning router qabul qiladi
+    2) Router esa controller ga yuzlanadi. Bu jarayonda controller bizga kelgan request taqdiri bilan shug'ullanadi. 
+        Controller ning asosiy vazifasi boshqaruvdan iborat bolganligi uchun unga kop mantiq yozilmaydi.
+    3) Controller lar, oz navbatida, tegishli service Modellarga request yuboradi => Malum bir vazifani bajar deydi
+    4) Modellar biznes logic ham deyiladi, yani asosiy mantiqlar shu modellarda yoziladi. 
+        Modellar 2 xil boladi (bu ikkalasi birga ishlidi): 
+            1. Service Model (Controller uchun xizmat qiladi)
+            2. Schema Model (Database bn bilan birgalikda ishlidi)
+        => Ishlashi: 1. Schema Model orqali biz Database dan malumot CRUD qilamiz va uni natijasini Service Modelga Beramiz;
+                     2. Service Model esa u malumotni Controllerga beradi
+                     3. Controler lar oziga kelgan data ni View (ejs) ga beradi 
+                     4. View esa tayyor HTML soutce ni controllerga uzatadi
+                     5. Controller bolsa routerlar orqali bizning browserimizga yuboradi
+
+3. Router dan kelgan malumotni Controller orqali handle qilish (boshqarish)
+    Kerakli folder lar ochish: 1. controllers
+                               2. models -> only for the service model
+                               3. schema -> only for the schema model
+                               4. libs > common.ts
+
+
+=> ESLATMA: controllerlarni doim Object orqali hozil qilamiz
+
+*/
--- src/train.ts
+++ src/train.ts
@@ -1,25 +1,66 @@
 console.log("---------> Welcome to the Training Page! <---------");
 
+// // C Task
+
+// Shunday function tuzing, u 2ta string parametr ega bolsin,
+//  hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin.
+//  MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true.
+
+// function checkContent(word1: string, word2: string) {
+//   word1 = word1.toLocaleLowerCase();
+//   word2 = word2.toLocaleLowerCase();
+
+//   let word1Elems: string[] = [];
+//   let word2Elems: string[] = [];
+
+//   for (let i = 0; i < word1.length; i++) {
+//     word1Elems.push(word1[i]);
+//   }
+
+//   for (let i = 0; i < word2.length; i++) {
+//     word2Elems.push(word2[i]);
+//   }
+
+//   const word1Sorted = word1Elems.sort().join();
+//   const word2Sorted = word2Elems.sort().join();
+
+//   return word1Sorted === word2Sorted;
+// }
+
+// const result: boolean = checkContent("mitgroup", "gmtiprou");
+// console.log(result);
+
+const sameWords = (first: string, second: string): boolean => {
+  if (first.length !== second.length) return false;
+
+  let a = first.split("").sort().join();
+  let b = second.split("").sort().join();
+
+  return a === b;
+};
+
+console.log(sameWords("52wba", "2aw5b"));
+
 /* H-TASK
     Shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, 
     faqat positive qiymatlarni olib string holatda return qilsin. 
     MASALAN: getPositive([1, -4, 2]) return qiladi "12".
 */
 
-function getPositive(userInput: number[]) {
-  let evenNums: number[] = [];
+// function getPositive(userInput: number[]) {
+//   let evenNums: number[] = [];
 
-  for (let i: number = 0; i < userInput.length; i++) {
-    if (userInput[i] >= 0) {
-      evenNums.push(userInput[i]);
-    }
-  }
+//   for (let i: number = 0; i < userInput.length; i++) {
+//     if (userInput[i] >= 0) {
+//       evenNums.push(userInput[i]);
+//     }
+//   }
 
-  return evenNums.join("");
-}
+//   return evenNums.join("");
+// }
 
-const result = getPositive([1, -4, 2]);
-console.log(result);
+// const result = getPositive([1, -4, 2]);
+// console.log(result);
 
 /* G-TASK
     Shunday function tuzingki unga integerlardan iborat array pass bolsin va 
