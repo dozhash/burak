cb64aaec6568a33e73a3e7b3cd6f6274012ea971
Shakhzod <shakhzod.ds@gmail.com>
2026-01-19 20:46:40 +0900

    feat: develop signup process

--- darsNotes/40_41.ts
+++ darsNotes/40_41.ts
@@ -11,7 +11,7 @@ ESLATMA: MemberService -> member controllerga ham restaran controllerga ham bird
 --> Loyihamizning Standartlari -> developing jarayonini biz uchun yengillashtiradi,
                               hamda jamoada birga ishlashimiz uchun muhum vositachi hisoblanadi.
 
-    Bunga Misol qilib: 1. logging mehanizmi => console.log("goHoe") kabi bizga kelayotgan request holatini koramiz
+    Bunga Misol qilib: 1. logging mehanizmi => console.log("goHome") kabi bizga kelayotgan request holatini koramiz
                        2. errorlarni handle qilish mehanizmi => try catch dan foydalanib
                        3. serverga kelayotgan turli request lar type ni korish => morgan@1.10.0 && @types/morgan ni ornatib
                           kabi standartlar nazarda tutiladi
@@ -30,7 +30,7 @@ ESLATMA: MemberService -> member controllerga ham restaran controllerga ham bird
 
 2-BO'LIM:
 --> request larimizning methodlari haqida:
-                            post metodli requestlarni ko'rib chiqamiz:
+                post metodli requestlarni ko'rib chiqamiz:
 
 ESLATMA: Browser by default faqat bitta methodga hizmat qiladi: get()
 
@@ -39,6 +39,51 @@ post() maqsadi --> malumotni o'zgartirishda (mutation) ishlatiladi
                    => bu method ni test qilishda POSTMAN ishlatiladi
 
 --> Response Turlari: send | json | redirect | end | render
+
+
+*/
+
+/* 41. Member - Service Model (admin signup)
+
+Dars Rejasi: 1. API o'zi nima va uning qanday turlari mavjud?
+             2. Adminka loyihasi uchun mo'ljallangan signup API ni to'liq develop qilamiz
+
+API - Aplication Programming Interface --> bu dasturlar bir-biri bilan gaplashishi uchun foydalanadigan umumiy tildir.
+
+Eng ko'p ishlatiladigan API turlari: 1. Traditional API (Burak loyihamizda)
+                                     2. Rest Api (Burak loyihamizda)
+                                     3. GraphQL API
+
+API REQUESTS:
+    1. TYPE > Traditional API | Rest API | GraphQL API
+    2. METHOD > GET | POST
+    3. STRUCTURE > header | body
+
+| API turi        | Kalit so‘z   | Nimani qaytaradi | Qanday bilasan               |
+| --------------- | ------------ | ---------------- | ---------------------------- |
+| Traditional API | **PAGE**     | HTML sahifa      | `render`, `redirect`, `form` |
+| REST API        | **RESOURCE** | JSON data        | `res.json`, GET/POST/PUT     |
+| GraphQL API     | **QUERY**    | So‘ralgan JSON   | `/graphql`, `query {}`       |
+
+1️⃣ HTML sahifami? → Traditional
+2️⃣ JSON + resource URL’mi? → REST
+3️⃣ Bitta endpoint + query’mi? → GraphQL
+
+Postman qanday ishlayabdi:  Postman
+                                ↓ (HTTP request)
+                            http://localhost:3000/login
+                                ↓
+                            Node + Express (server)
+                                ↓
+                            router → controller
+                                ↓
+                            console.log(req.body)
+
+                            👉 VS Code terminalida chiqyapti 🎯
+
+findOne().exec() --> Query lar ketma-ketligini to'xtatish maqsadida
+
+
 
 
 */
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,5 +1,41 @@
+import MemberModel from "../schema/Member.model";
+import { Member, MemberInput } from "../libs/types/member";
+import Errors, { HttpCode, Message } from "../libs/Errors";
+import { MemberType } from "../libs/enums/member.enum";
+
 class MemberService {
-  constructor() {}
+  private readonly memberModel;
+
+  constructor() {
+    this.memberModel = MemberModel;
+  }
+
+  public async processSignup(input: MemberInput): Promise<Member> {
+    // Faqat 1 ta restaurant bolishi
+    const exist = await this.memberModel
+      .findOne({
+        memberType: MemberType.RESTAURANT,
+      })
+      .exec();
+
+    console.log("Exist:", exist);
+
+    if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+    // MongoDB ga malumotni qo'shish
+    try {
+      const result = await this.memberModel.create(input);
+
+      // const tempresult = new this.memberModel(input);
+      // const result = await tempresult.save();
+
+      result.memberPassword = "";
+
+      return result;
+    } catch (err) {
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    }
+  }
 }
 
 export default MemberService;
